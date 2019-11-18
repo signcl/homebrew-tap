930f2cf5514ab0d3cc536be6e54362cc83ff4dfd
goreleaserbot <goreleaser@carlosbecker.com>
2019-11-18 20:27:51 +0800

    Brew formula update for bayes version v0.5.9

--- Formula/bayes.rb
+++ Formula/bayes.rb
@@ -2,16 +2,16 @@
 class Bayes < Formula
   desc "CLI command-line tools for OpenBayes console"
   homepage "https://openbayes.com"
-  version "0.5.8"
+  version "0.5.9"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/signcl/bayes-releases/releases/download/v0.5.8/bayes_darwin_amd64.zip"
-    sha256 "adf2be2315b786dfb09fa8f600e9ea7534397da6976e0deace3dee860af04342"
+    url "https://github.com/signcl/bayes-releases/releases/download/v0.5.9/bayes_darwin_amd64.zip"
+    sha256 "ebf6d62e19e309d93be74da57e47892a884e125a5d1d669a91505c27ac553067"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/signcl/bayes-releases/releases/download/v0.5.8/bayes_linux_amd64.zip"
-      sha256 "bd84796bd5b43c6d751eea86a258194241658fdaedbf14bbc8fe3dcd4b005d35"
+      url "https://github.com/signcl/bayes-releases/releases/download/v0.5.9/bayes_linux_amd64.zip"
+      sha256 "b63487e1d4c85712513e9e5fa0483be942c731e6002bf8e077363dd7e4b77a8d"
     end
   end
 
